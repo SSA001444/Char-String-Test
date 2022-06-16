8933c6a492bd53821963efce61082f599059a2b3
Sergey Skripka <97537922+SSA001444@users.noreply.github.com>
2022-06-16 14:08:42 +0000

    Pievienota pareizo un nepareizo atbilžu sistēma

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     int punkts = 0;
+    int index = 0;
     int atbl;
+    int[] nepatbl = new int[10];
     Scanner scan = new Scanner(System.in);
     System.out.println("Sveiki! Šis ir tests, lai pārbaudītu zināšanas par simboliem un simboliem virknēm. Test sastāv no 10 jautājumiem.\n\n");
 
@@ -10,41 +12,88 @@ class Main {
     atbl = scan.nextInt();
     if(atbl == 3)
       punkts++;
+    else{
+      nepatbl[index] = 1;
+      index++;
+    }
     System.out.println("\n2. Ja jums ir jāievada unikods char, ko jūs izmantosit?\n 1.char a = 0041\n 2.char a = 41\n 3.char a = \\u0041 \n 4.char a = '\\u0041'\n\n");
     atbl = scan.nextInt();
       if(atbl == 4)
       punkts++;
+      else{
+      nepatbl[index] = 2;
+      index++;
+    }
      System.out.println("\n3. Kā uzzināt simbola Unicode ar String?\n 1.codePointAt()\n 2. Nav iespējams\n 3.(int)String\n 4. 1. un 3. pareizi\n\n");
      atbl = scan.nextInt();
       if(atbl == 1)
       punkts++;
+      else{
+      nepatbl[index] = 3;
+      index++;
+    }
     System.out.println("\n4. Ka parbaudē ir tas simbols(i) String vai ne?\n 1.Meklēt paši\n 2. Nav iespējams\n 3.contains()\n 4.indexOf()\n\n");
          atbl = scan.nextInt();
       if(atbl == 3)
       punkts++;
+      else{
+      nepatbl[index] = 4;
+      index++;
+    }
     System.out.println("\n5. Ka uzzināt index simbola String?\n 1.Meklēt paši\n 2.lastIndeOf()\n 3. 2. un 4. pareizi\n 4.indexOf()\n\n");
          atbl = scan.nextInt();
       if(atbl == 3)
       punkts++;
+      else{
+      nepatbl[index] = 5;
+      index++;
+    }
     System.out.println("\n6. Ko funkcija isEmpty() dara?\n 1.Notīra viss String\n 2.Pārbauda,vai String tukšuma\n 3. Šī funkcija neeksistē\n 4.Šeit nav pareizas atbildes\n\n");
          atbl = scan.nextInt();
       if(atbl == 2)
       punkts++; 
+      else{
+      nepatbl[index] = 6;
+    index++;
+    }
     System.out.println("\n7. Ko funkcija isDigit() dara?\n 1.Pārbauda,va ir skaitlis String\n 2.Pārbauda,vai ir skaitlis char\n 3. Šī funkcija neeksistē\n 4.Šeit nav pareizas atbildes\n\n");
        atbl = scan.nextInt();
       if(atbl == 2)
       punkts++; 
+      else{
+      nepatbl[index] = 7;
+    index++;
+    }
     System.out.println("\n8. Ko funkcija toLowerCase() dara?\n 1.Pārbauda,vai tas ir mazāka reģistrēties\n 2.Pārbauda,vai tas ir lielāka reģistrēties\n 3. Maiņa reģistrēties uz mazāko \n 4.Maiņa reģistrēties uz lielāko\n\n");
       atbl = scan.nextInt();
       if(atbl == 3)
       punkts++; 
+      else{
+      nepatbl[index] = 8;
+    index++;
+    }
      System.out.println("\n9.Kā savienot divas virknes?\n 1.split()\n 2.contains()\n 3.compareTo\n 4.concat()\n\n");
       atbl = scan.nextInt();
       if(atbl == 4)
       punkts++; 
+      else{
+      nepatbl[index] =9;
+    index++;
+    }
      System.out.println("\n10.Kā sadalīt divas virknes?\n 1.split()\n 2.contains()\n 3.compareTo\n 4.concat()\n\n");
       atbl = scan.nextInt();
       if(atbl == 1)
       punkts++; 
+      else{
+      nepatbl[index] = 10;
+    index++;
+    }
+    System.out.println("Pareizas atbildes: " + punkts + "\n");
+    System.out.println("Nepareizi atbildēti jautājumi:");
+        for(int ind : nepatbl)
+          if(ind!=0)
+      System.out.println(ind+".");
+    scan.close();
+    
   }
 }
